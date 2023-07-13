237028c188e99b2c6f46c5b3899d5d108d2f1806
mrbitcoiner <mrbitcoiner@tutanota.com>
2023-07-13 23:50:24 +0000

    * Redirect tor logs to /dev/null

--- containers/bitcoind/volume/scripts/init.sh
+++ containers/bitcoind/volume/scripts/init.sh
@@ -5,7 +5,9 @@ set -e
 su -c '/app/scripts/bitcoind_build.sh' ${CONTAINER_USER}
 /app/scripts/gracefully_shutdown.sh install
 if echo "${TOR_PROXY}" | grep '^enabled$' > /dev/null; then
-  su -c 'tor &' ${CONTAINER_USER}
+  printf 'Starting tor\n'
+  sleep 1
+  su -c 'tor > /dev/null &' ${CONTAINER_USER}
 fi
 su -c '/app/scripts/bitcoind_setup.sh' ${CONTAINER_USER}
 su -c 'bitcoind' ${CONTAINER_USER}
