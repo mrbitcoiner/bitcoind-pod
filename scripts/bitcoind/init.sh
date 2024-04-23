f536e14f69a827ffb643bfc39b9d199ee4d42652
_ <_>
2024-04-23 17:01:29 +0000

    * Container init   - Make sure that there's a copy of the bitcoin-cli binary on data directory

--- scripts/bitcoind/init.sh
+++ scripts/bitcoind/init.sh
@@ -7,6 +7,7 @@ setup_tor(){
 	[ "${TOR_PROXY}" == "enabled" ] && tor 1>/dev/null || return 0 & 
 }
 setup_bitcoind(){
+	[ -e "/data/bitcoin-cli" ] || cp -a /bitcoin/bin/bitcoin-cli /data/bitcoin-cli
 	/static/scripts/bitcoind/setup-bitcoind.sh
 	bitcoind
 }
